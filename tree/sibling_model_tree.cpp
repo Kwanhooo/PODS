aa0de49e37e626b1dca449a3beca0c5884e78d0a
Kwanhooo <kwanhooo@foxmail.com>
2023-08-09 17:20:00 +0800

    feat: 树(森林)的孩子兄弟表示法

---
+++ tree/sibling_model_tree.cpp
@@ -0,0 +1,6 @@
+//
+// 孩子兄弟表示法
+// 测试
+//
+
+#include "sibling_model_tree.h"
---
+++ tree/sibling_model_tree.h
@@ -0,0 +1,27 @@
+//
+// 孩子兄弟表示法
+// 实现
+//
+
+#ifndef TREE_SIBLING_MODEL_TREE_H
+#define TREE_SIBLING_MODEL_TREE_H
+
+#include "element.h"
+#include "child_model_tree.h"
+
+namespace sibling_model_tree {
+
+    inline void test_sibling_model_tree();
+
+    typedef struct TreeNode {
+        Element data;
+        struct TreeNode *first_child, *next_sibling;
+    } TreeNode, *Tree;
+
+    typedef struct ForestNode {
+        Tree tree;
+        ForestNode *first_child, *next_sibling;
+    } ForestNode, *Forest;
+}
+
+#endif //TREE_SIBLING_MODEL_TREE_H
